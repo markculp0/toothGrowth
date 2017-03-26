ec481b0ea99527b2978aa8f4565e2f7b8881dd50
Mark Culp <markculp0@gmail.com>
2017-03-25 22:14:28 -0500

    1st commit

---
+++ toothGrowth.R
@@ -0,0 +1,19 @@
+# Tooth Growth Analysis
+# Statistical Inference Class
+# Mark Culp
+# 3/22/2017
+
+## Overview:
+
+library(datasets)
+data("ToothGrowth")
+
+
+
+## Exploratory analysis
+
+## Data summary 
+
+## Comparison of tooth growth by supp and dose
+
+## Conclusions and assumptions
