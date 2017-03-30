5c5bf1df10edbb1c2692f4f8d47fb2bf0f5eb035
Mark Culp <markculp0@gmail.com>
2017-03-29 21:27:19 -0500

    Exploratory code added.

---
+++ README.md
@@ -0,0 +1,9 @@
+# toothGrowth class project
+## Statistical Inference, Johns Hopkins
+## Mark Culp
+## March 2017
+Basic inferential data analysis of the R dataset "ToothGrowth"
+
+
+
+
--- toothGrowth.R
+++ toothGrowth.R
@@ -5,15 +5,40 @@
 
 ## Overview:
 
+# This analysis examines the effects of vitamin C on 
+# tooth growth in guinea pigs.  Each of the 60 guinea 
+# pigs studied received one of three doses of vitamin
+# C per day: 0.5, 1.0, or 2.0 mg/day.  There were two 
+# delivery methods: orange juice (OJ) and ascorbic 
+# acid (VC).  The response measures the growth in 
+# odontoblasts (cells responsible for tooth growth).
+
 library(datasets)
 data("ToothGrowth")
 
-
-
+## ----------------------------
 ## Exploratory analysis
 
+# List length of rows / columns
+dim(ToothGrowth)
+
+# Determine column data types
+str(ToothGrowth)
+
+# Range of tooth growth
+range(ToothGrowth$len)
+
+# Levels and counts of delivery methods/supplements
+table(ToothGrowth$supp)
+
+# Levels and counts of dosages
+table(ToothGrowth$dose)
+
+## ----------------------------
 ## Data summary 
 
+## ----------------------------
 ## Comparison of tooth growth by supp and dose
 
+## ----------------------------
 ## Conclusions and assumptions
